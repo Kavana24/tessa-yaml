fc58a2c117065e5252c333898d886c339b164c6a
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2019-12-09 17:23:04 +0530

    Create Cellranger_csv.sh

---
+++ Cellranger_csv.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+input="$1"
+
+
+exec < $input || exit 1
+read header # read (and ignore) the first line
+while IFS=, read heading1 heading2 heading3 heading4 heading5; do
+    id="$heading1"
+    transcriptome="$heading2"
+    fastqs="$heading3"
+    sample="$heading4"
+	gcsbucket="heading5"
+    sample1=`echo $sample | sed 's/"""//g'`
+    echo id = $id
+    echo transcriptome = $transcriptome
+    echo fastqs = $fastqs
+	#echo sample = $sample
+	echo sample1 = $sample1
+	echo gcsbucket = $gcsbucket
+	
+size=`gsutil du -s $gcsbucket`
+sizespilt="$(cut -d ' ' -f1 <<<"$size")"
+sizemul=`expr $sizespilt \* 2`
+PVCSIZE=`expr $sizemul / 1073741824`
+	    
+java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build cellranger-pipeline -p size=$PVCSIZE -p id=$id -p transcriptome=$transcriptome -p sample=$sample1 -p fastqs=$fastqs  -p gcsbucket=$gcsbucket
+	
+done

9c68fa23a9fe64e93cdfe93d2ecf85eb9e51f677
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2019-12-10 11:41:09 +0530

    Update Cellranger_csv.sh

--- Cellranger_csv.sh
+++ Cellranger_csv.sh
@@ -19,11 +19,12 @@ while IFS=, read heading1 heading2 heading3 heading4 heading5; do
 	echo gcsbucket = $gcsbucket
 	
 size=`gsutil du -s $gcsbucket`
+echo $size
 sizespilt="$(cut -d ' ' -f1 <<<"$size")"
 sizemul=`expr $sizespilt \* 2`
 PVCSIZE=`expr $sizemul / 1073741824`
 echo $PVCSIZE
 	    
-java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build cellranger-pipeline -p size=$PVCSIZE -p id=$id -p transcriptome=$transcriptome -p sample=$sample1 -p fastqs=$fastqs -p gcsbucket=$gcsbucket
+#java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build cellranger-pipeline -p size=$PVCSIZE -p id=$id -p transcriptome=$transcriptome -p sample=$sample1 -p fastqs=$fastqs -p gcsbucket=$gcsbucket
 	
 done

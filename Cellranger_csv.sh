3c93aa113a3579bdb16cdd84bc871a9cde208501
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2019-12-10 11:54:04 +0530

    Update Cellranger_csv.sh

--- Cellranger_csv.sh
+++ Cellranger_csv.sh
@@ -9,21 +9,20 @@ while IFS=, read heading1 heading2 heading3 heading4 heading5; do
     transcriptome="$heading2"
     fastqs="$heading3"
     sample="$heading4"
-	gcsbucket="$heading5"
+    gcsbucket="$heading5"
     sample1=`echo $sample | sed 's/"""//g'`
     echo id = $id
     echo transcriptome = $transcriptome
     echo fastqs = $fastqs
 	#echo sample = $sample
-	echo sample1 = $sample1
-	echo gcsbucket = $gcsbucket
-	
-size=`gsutil du -s $gcsbucket`
-echo $size
-sizespilt="$(cut -d ' ' -f1 <<<"$size")"
-sizemul=`expr $sizespilt \* 2`
-PVCSIZE=`expr $sizemul / 1073741824`
-echo $PVCSIZE
+    echo sample1 = $sample1
+    echo gcsbucket = $gcsbucket
+    size=`gsutil du -s $gcsbucket`
+    echo $size
+    sizespilt="$(cut -d ' ' -f1 <<<"$size")"
+    sizemul=`expr $sizespilt \* 2`
+    PVCSIZE=`expr $sizemul / 1073741824`
+    echo $PVCSIZE
 	    
 #java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build cellranger-pipeline -p size=$PVCSIZE -p id=$id -p transcriptome=$transcriptome -p sample=$sample1 -p fastqs=$fastqs -p gcsbucket=$gcsbucket
 	

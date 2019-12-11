61abe039a4f52ea02d6b8c347fc3449608e2b8f3
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2019-12-11 12:19:11 +0530

    Update Cellranger_csv.sh

--- Cellranger_csv.sh
+++ Cellranger_csv.sh
@@ -20,7 +20,7 @@ while IFS=, read heading1 heading2 heading3 heading4 heading5; do
     size1=`gsutil du -s $gcsbucket`
     echo $size1
     sizespilt="$(cut -d ' ' -f1 <<<"$size1")"
-    sizemul=`expr $sizespilt \* 2`
+    sizemul=`expr $sizespilt \* 4`
     PVCSIZE=`expr $sizemul / 1073741824`
     echo $PVCSIZE
 java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build Testing_jenkins_cli -p param1=hello

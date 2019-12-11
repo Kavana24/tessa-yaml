67a573aec4ad34940664acc8d1aaa6e3898cdd98
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2019-12-11 16:36:46 +0530

    Update testingloop.sh

--- testingloop.sh
+++ testingloop.sh
@@ -3,8 +3,12 @@
  i=1
 while [ $x -le 3 ]
 do
+timestamp=$(date +%s)
+echo $timestamp
 java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build Testing_jenkins_cli -p param1=hello
 x=$(($x+1))
 echo $i
 i=$(($i+1))
+timestamp=$(date +%s)
+echo $timestamp
 done

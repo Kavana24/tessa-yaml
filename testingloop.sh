2f354dab3c0aed8322a47d45befe4193dd5986ac
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2019-12-11 12:27:50 +0530

    Update testingloop.sh

--- testingloop.sh
+++ testingloop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  x=1
- #i=0
+ i=1
 while [ $x -le 3 ]
 do
 java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build Testing_jenkins_cli -p param1=hello
 x=$(($x+1))
-#i=$(($i+1))
-#echo $i
+echo $i
+i=$(($i+1))
 done

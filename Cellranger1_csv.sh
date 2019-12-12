6ee39c7bd7a159a98cf0f24f772c4848a6b17f4d
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2019-12-12 14:17:12 +0530

    Update Cellranger1_csv.sh

--- Cellranger1_csv.sh
+++ Cellranger1_csv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 input="$1"
-
+input1="sample3.csv"
 dos2unix $input
 exec < $input || exit 1
-read header # read (and ignore) the first line
-while IFS=, read heading1 heading2 heading3 heading4 heading5 
+awk 'NR>1' $input > $input1
+while IFS=',' read -r heading1 heading2 heading3 heading4 <&3;
 do
 id="$heading1"
 transcriptome="$heading2"
@@ -21,8 +21,10 @@ echo gcsbucket = $gcsbucket
 size1=`gsutil du -s $gcsbucket`
 echo $size1
 sizespilt="$(cut -d ' ' -f1 <<<"$size1")"
-sizemul=`expr $sizespilt \* 3`
+sizemul=`expr $sizespilt \* 4`
 PVCSIZE=`expr $sizemul / 1073741824`
 echo $PVCSIZE
-java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build  Cellranger-pipeline-freestyle -p size=$PVCSIZE -p id=$id -p transcriptome=$transcriptome -p sample=$sample1 -p fastqs=$fastqs -p gcsbucket=$gcsbucket
-done
+java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build  Cellranger-pipeline -p size=$PVCSIZE -p id=$id -p transcriptome=$transcriptome -p sample=$sample1 -p fastqs=$fastqs -p gcsbucket=$gcsbucket
+done 3< "$input1"
+rm sample3.csv
+

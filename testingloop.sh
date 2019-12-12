2a8efbf4f21569db2f92686e5804a5782676f8c3
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2019-12-12 12:05:51 +0530

    Update testingloop.sh

--- testingloop.sh
+++ testingloop.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
  x=1
- i=1
+ #i=1
 while [ $x -le 3 ]
 do
-timestamp=$(date +%s)
-echo $timestamp
+#timestamp=$(date +%s)
+#echo $timestamp
 java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build cellranger-pipeline -p size=100 -p id=1k_PBMC_PUC1 -p transcriptome=/mounttest/PUC_test/reference/refdata-cellranger-GRCh38-3.0.0 -p sample="pbmc_1k_v3" -p fastqs=/mounttest/PUC_test/pbmc_1k_v3_fastqs  -p gcsbucket=gs://testinggenomic/PUC_test
-echo $i
-i=$(($i+1))
-timestamp=$(date +%s)
-echo $timestamp
+$x=$(($x+1))
+echo $x
+#echo $i
+#$i=$(($i+1))
+#timestamp=$(date +%s)
+#echo $timestamp
 done

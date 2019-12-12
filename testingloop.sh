c4b6a8dc90111b1a3eeb6dc4e24646257abaabb7
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2019-12-12 11:47:45 +0530

    Update testingloop.sh

--- testingloop.sh
+++ testingloop.sh
@@ -5,7 +5,7 @@ while [ $x -le 3 ]
 do
 timestamp=$(date +%s)
 echo $timestamp
-java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build Testing_jenkins_cli -p param1=hello
+java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build cellranger-pipeline -p size=$PVCSIZE -p id=$id -p transcriptome=$transcriptome -p sample=$sample1 -p fastqs=$fastqs  -p gcsbucket=$gcsbucket
 x=$(($x+1))
 echo $i
 i=$(($i+1))

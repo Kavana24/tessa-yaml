5a7c75c017f9182fd2244ba8b5d08a082d0a22a1
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2020-01-06 14:52:49 +0530

    Update Cellranger_csv.sh

--- Cellranger_csv.sh
+++ Cellranger_csv.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+#read the csv file
 input="$1"
 input1="sample3.csv"
+# converting csv to linuxformat
 dos2unix $input
+#ignoring the heading and storing to another file
 exec < $input || exit 1
 awk 'NR>1' $input > $input1
 #while IFS=',' read -r id transcriptome fastqs sample gcsbucket <&3;
@@ -17,17 +20,21 @@ awk 'NR>1' $input > $input1
 #echo $input
 #ctls Cellrangercount1.csv 3 1
 
+#reading the values from csv
 inputLength=`csvtool height $input1`
 for ((i=0; i<$inputLength ;i++))
 do
 csvtool drop $i $input1 | csvtool take 1 - > output.csv
-id=`csvtool format '%(1)\n' output.csv`
-transcriptome=`csvtool format '%(2)\n' output.csv`
-fastqs=`csvtool format '%(3)\n' output.csv`
-gcsbucket=`csvtool format '%(5)\n' output.csv`
-sample=`csvtool format '%(4)\n' output.csv`
+expid=`csvtool format '%(1)\n' output.csv`
+id=`csvtool format '%(2)\n' output.csv`
+transcriptome=`csvtool format '%(3)\n' output.csv`
+fastqs=`csvtool format '%(4)\n' output.csv`
+gcsbucket=`csvtool format '%(6)\n' output.csv`
+sample=`csvtool format '%(5)\n' output.csv`
+#parse the sample value, remove double qoutes
 sample1=`echo $sample | sed 's/"//g'`
 #csvtool format '%(4)\n' output.csv
+echo expid = $expid
 echo id = $id
 echo transcriptome = $transcriptome
 echo fastqs = $fastqs
@@ -36,11 +43,19 @@ echo sample1 = $sample1
 echo gcsbucket = $gcsbucket
 size1=`gsutil du -s $gcsbucket`
 echo $size1
+#calculating the size for the k8-pvc
 sizespilt="$(cut -d ' ' -f1 <<<"$size1")"
 sizemul=`expr $sizespilt \* 4`
 PVCSIZE=`expr $sizemul / 1073741824`
 echo $PVCSIZE
+#Checking for id in expid sql table
+count=`mysql -hseurat-test -P3306 -ujenkinsuser -pjenkins123 -D tessa_output -B -N -e "Select count(Sample_ID) from $expid where Sample_ID='$id'";`
+if [ $count -eq 1 ]
+then
 java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build  Cellranger-pipeline -p size=$PVCSIZE -p id=$id -p transcriptome=$transcriptome -p sample=$sample1 -p fastqs=$fastqs -p gcsbucket=$gcsbucket
+else
+echo "Invalid ID for the corresponding Experiment"; 
+fi
 done
 rm output.csv
 rm $input1

9dc10d9797cc2e3495aa05ea7e9282f1cbca0aee
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2020-01-17 17:45:08 +0530

    Update Seurat1_success_failure.sh

--- Seurat/Seurat1_success_failure.sh
+++ Seurat/Seurat1_success_failure.sh
@@ -2,19 +2,18 @@
 BUILDID="$1"
 echo $BUILDID
 JENKINSJOBID="$2"
-ATCINPUTFOLDERPATH="$3"
-VTSINPUTFOLDERPATH="$4"
+EXPID="$3"
+var1=`var1=`echo "$a" | awk '{print tolower($0)}'`
+expidlower=`echo "$var1" | tr '_' '-'`
+K8JOBNAME="$expidlower"-"seurat1"-"$BUILDID"
 SEURATOUTGCSBUCKET="gs://testinggenomic/Seurat_output"
-SEURATOUTFOLDER=seurat1out-$BUILDID
+SEURATOUTFOLDER="seurat1out"-"$EXPID"-"$BUILDID"
 
-if mkdir $SEURATOUTFOLDER && cd $SEURATOUTFOLDER &&  Rscript /mounttest/Taka/Scripts/Seurat1.R $ATCINPUTFOLDERPATH $VTSINPUTFOLDERPATH && ls && gsutil cp -r ../$SEURATOUTFOLDER $SEURATOUTGCSBUCKET ;
+if mkdir $SEURATOUTFOLDER && cd $SEURATOUTFOLDER &&  Rscript /mounttest/Seurat/Seurat1.R && gsutil cp -r ../$SEURATOUTFOLDER $SEURATOUTGCSBUCKET ;
 then
 echo "working"
 java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build Seurat1-success-notification -p jenkinsjobID=$JENKINSJOBID -p buildid=$BUILDID -p outputgcsbucket=$SEURATOUTGCSBUCKET/$SEURATOUTFOLDER -p seuratoutfolder=$SEURATOUTFOLDER
-
 else
-
 echo "not working"
 java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build Seurat-failure-notification -p jenkinsjobID=$JENKINSJOBID-$BUILDID -p k8jobID=seuart1-$BUILDID
-
 fi

8afa360699a4d3221476638625bc70e3f8b71e3e
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2020-01-21 15:49:25 +0530

    Update Seurat1_success_failure.sh

--- Seurat/Seurat1_success_failure.sh
+++ Seurat/Seurat1_success_failure.sh
@@ -10,7 +10,7 @@ podname=`hostname`;
 SEURATOUTGCSBUCKET="gs://testinggenomic/Seurat_output"
 SEURATOUTFOLDER="seurat1out"-"$EXPID"-"$BUILDID"
 
-if mkdir $SEURATOUTFOLDER && cd $SEURATOUTFOLDER &&  Rscript /mounttest/Seurat/Seurat1.R && gsutil cp -r ../$SEURATOUTFOLDER $SEURATOUTGCSBUCKET ;
+if mkdir $SEURATOUTFOLDER && cd $SEURATOUTFOLDER &&  Rscript /mounttest/gitrepo/Seurat/Seurat1.R && gsutil cp -r ../$SEURATOUTFOLDER $SEURATOUTGCSBUCKET ;
 then
 echo "working"
 java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build Seurat1-success-notification -p jenkinsjobID=$JENKINSJOBID -p buildid=$BUILDID -p outputgcsbucket=$SEURATOUTGCSBUCKET/$SEURATOUTFOLDER -p seuratoutfolder=$SEURATOUTFOLDER -p k8jobname=$K8JOBNAME -p podname=$podname -p expid=$EXPID

f790b6c4daccc5c75c583e194f26e9432d8e234a
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2020-01-23 15:35:38 +0530

    Update Seurat2_success_failure.sh

--- Seurat/Seurat2_success_failure.sh
+++ Seurat/Seurat2_success_failure.sh
@@ -12,7 +12,7 @@ SEURATOUTGCSBUCKET="gs://testinggenomic/Seurat_output"
 SEURAT2OUTFOLDER="seurat2out"-"$EXPID"-"$BUILDID"
 RDSINPUTFILEPATH="/mounttest/$SEUART1RDSFOLDERPATH/$EXPID.rds"
 
-if mkdir $SEURAT2OUTFOLDER && cd $SEURAT2OUTFOLDER && Rscript /mounttest/gitrepo/Seurat/Seurat2.R $RDSINPUTFILEPATH ; ls ; gsutil cp -r ../$SEURAT2OUTFOLDER $SEURATOUTGCSBUCKET;
+if mkdir $SEURAT2OUTFOLDER && cd $SEURAT2OUTFOLDER && Rscript /mounttest/gitrepo/Seurat/Seurat2.R $RDSINPUTFILEPATH && ls && gsutil cp -r ../$SEURAT2OUTFOLDER $SEURATOUTGCSBUCKET;
 then
 echo "working"
 java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build Seurat2-success-notification -p buildid=$BUILDID -p seurat1outgcsbucket=$SEURAT1OUTGCSBUCKET -p seurat1rdsfolderpath=$SEUART1RDSFOLDERPATH -p jenkinsjobID=$JENKINSJOBNAME-$BUILDID -p k8jobname=$K8JOBNAME -p podname=$podname -p expid=$EXPID outputgcsbucket=$SEURATOUTGCSBUCKET/$SEURAT2OUTFOLDER -p seurat2outfolder=$SEURAT2OUTFOLDER 

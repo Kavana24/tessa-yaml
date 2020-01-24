030c0858d5e9fc74ac7502057f7e67c272fdfc69
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2020-01-24 14:21:02 +0530

    Update Seurat1csv.sh

--- Seurat/Seurat1csv.sh
+++ Seurat/Seurat1csv.sh
@@ -9,57 +9,78 @@ declare -a sampleIdArr
 declare -a opGcsArr
 declare -a originalgcsbucArr
 for ((i=0; i<$inputLength ; i++))
-do
-csvtool drop $i $input1 | csvtool take 1 - > output.csv
-expid=`csvtool format '%(1)\n' output.csv`
-outputgcsbucket=`csvtool format '%(2)\n' output.csv`
-outputbucketvalue=`mysql -h10.60.2.8 -P3306 -ujenkinsuser -pjenkins123 -D tessa_output -B -N -e "Select count(Outputgcsbucket) from cellranger_new_output_details where Outputgcsbucket='$outputgcsbucket' AND Cellranger_status = 'Success' AND ApprovalStatus = 'Approved'";`
-#echo $outputbucketvalue
-if [ $outputbucketvalue -eq 1 ]
-then
-sampleID=`mysql -h10.60.2.8 -P3306 -ujenkinsuser -pjenkins123 -D tessa_output -B -N -e "Select Sample_ID_BuildID  from cellranger_new_output_details where Outputgcsbucket = '$outputgcsbucket'";`
+        do
+        csvtool drop $i $input1 | csvtool take 1 - > output.csv
+        expid=`csvtool format '%(1)\n' output.csv`
+        outputgcsbucket=`csvtool format '%(2)\n' output.csv`
+        outputbucketvalue=`mysql -hseurat-test -P3306 -ujenkinsuser -pjenkins123 -D tessa_output -B -N -e "Select count(Outputgcsbucket) from cellranger_new_output_details where Outputgcsbucket='$outputgcsbucket' AND Cellranger_status = 'Success' AND ApprovalStatus = 'Approved'";`
 
-sampleIdArr=("${sampleIdArr[@]}" "$sampleID")
-
-originalgcsbucArr=("${originalgcsbucArr[@]}" "$outputgcsbucket")
-opgcsbase=`basename $outputgcsbucket`;
-
-alteredopgcs="/mounttest/"$opgcsbase"/outs/filtered_feature_bc_matrix"
-
-opGcsArr=("${opGcsArr[@]}" "$alteredopgcs")
-else
-echo "Output GCS bucket not present in Cellranger Output table"
-fi
-done
+        if [ $outputbucketvalue -eq 1 ]
+                then
+                sampleID=`mysql -hseurat-test -P3306 -ujenkinsuser -pjenkins123 -D tessa_output -B -N -e "Select Sample_ID_BuildID  from cellranger_new_output_details where Outputgcsbucket = '$outputgcsbucket'";`
+                expidnew=`mysql -hseurat-test -P3306 -ujenkinsuser -pjenkins123 -D tessa_output -B -N -e "Select ExperimentID  from cellranger_new_output_details where Outputgcsbucket = '$outputgcsbucket'";`
+                sampleIdArr=("${sampleIdArr[@]}" "$sampleID")
+                originalgcsbucArr=("${originalgcsbucArr[@]}" "$outputgcsbucket")
+                opgcsbase=`basename $outputgcsbucket`;
+                alteredopgcs="/mounttest/"$opgcsbase"/outs/filtered_feature_bc_matrix"
+                opGcsArr=("${opGcsArr[@]}" "$alteredopgcs")
+                else
+                echo "Output GCS bucket not present in Cellranger Output table"
+                fi
+        done
 printf '%s\n'  "${sampleIdArr[@]}";
 printf '%s\n'  "${opGcsArr[@]}";
 rm output.csv
 rm $input1
-function seurat() {
-   arr=("$@")
-   len=${#sampleIdArr[@]}
+seurat(){
+        arr=("$@")
+                declare -a IDArr=("${!1}")
+        declare -a BucketArr=("${!2}")
+        len=${#IDArr[@]}
 for ((i=0; i<$len; i++))
-do
-echo "${sampleIdArr[i]} <- Read10X(data.dir=\""${opGcsArr[i]}"\")"
-done
-for ((j=0; j<$len; j++))
-do
-echo "${sampleIdArr[j]} <- CreateSeuratObject(counts=${sampleIdArr[j]}, project=\""${sampleIdArr[j]}"\")"
-done
-for ((k=0; k<$len-1; k++))
-do
-echo "dat_1 <- merge(${sampleIdArr[k]}, y=${sampleIdArr[k+1]}, add.cell.ids=c(\""${sampleIdArr[k]}"\",\""${sampleIdArr[k+1]}"\"), project=\"""Taka_HER2CAR""\")"
-done
+                do
+                echo "${IDArr[i]} <- Read10X(data.dir=\"$"{BucketArr[i]}"\")"
+        done
+        for ((j=0; j<$len; j++))
+                do
+                echo "${IDArr[j]} <- CreateSeuratObject(counts=${IDArr[j]}, project=\""${IDArr[j]}"\")"
+        done
+
+        if [ $len -eq 2 ]
+        then
+                for ((k=0; k<$len-1; k++))
+                        do
+                       # echo "$expidnew inside for loop"
+                        echo "dat_1 <- merge(${IDArr[k]}, y=${IDArr[k+1]}, add.cell.ids=c(\""${IDArr[k]}"\",\""${IDArr[k+1]}"\"),project=\""project-name"\")"
+                done
+        else
+                xstring=${IDArr[0]}
+                for ((i=1; i<$len; i++))
+                        do
+                        initystring=$initystring"${IDArr[i]},"
+                        ystring=`echo $initystring | sed 's/,$//'`
+                done
+                for ((j=0; j<$len; j++))
+                        do
+                        initaddcellstring=$initaddcellstring\""${IDArr[j]}"\",
+                        addcellstring=`echo $initaddcellstring | sed 's/,$//'`
+                done
+                echo "dat_1 <- merge(x = $xstring, y = c($ystring), add.cell.ids=c($addcellstring), project=\""project-name"\")"
+        fi
 }
 Rscript="Seurat$RANDOM.txt"
-seurat "${sampleIdArr[@]} ${opGcsArr[@]}" > $Rscript
+seurat sampleIdArr[@] opGcsArr[@]  > $Rscript
 sed -i "3r $Rscript" /mounttest/gitrepo/Seurat/Seurat1.R
-
-function bucketcopy() {
-arr=("$@")
-for bucket in ${arr[@]}
-do
-`gsutil cp -r $bucket /mounttest/`
-done
+`sed -i 's/project-name/'$expidnew'/g' Seurat.R`
+bucketcopy() {
+declare -a copybuckArr=("${!1}")
+declare -a copybuckArr=("${!1}")
+for bucket in ${copybuckArr[@]}
+        do
+        `gsutil cp -r $bucket /mounttest/`
+        done
 }
-bucketcopy "${originalgcsbucArr[@]}"
+#echo $expid
+bucketcopy originalgcsbucArr[@]
+rm $Rscript
+    

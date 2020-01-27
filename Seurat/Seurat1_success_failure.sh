8f77d8327e108c39dcce5f25a576bba3aceabbd9
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2020-01-27 11:45:08 +0530

    Update Seurat1_success_failure.sh

--- Seurat/Seurat1_success_failure.sh
+++ Seurat/Seurat1_success_failure.sh
@@ -17,6 +17,6 @@ mysql -h10.60.2.8 -P3306 -ujenkinsuser -pjenkins123 -D tessa_output -e "INSERT I
 java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build Seurat1-success-notification -p jenkinsjobID=$JENKINSJOBID -p buildid=$BUILDID -p outputgcsbucket=$SEURATOUTGCSBUCKET/$SEURATOUTFOLDER -p seurat1outfolder=$SEURATOUTFOLDER -p k8jobname=$K8JOBNAME -p podname=$podname -p expid=$EXPID
 else
 echo "not working"
-mysql -h10.60.2.8 -P3306 -ujenkinsuser -pjenkins123 -D tessa_output -e "INSERT INTO seurat1_output_details(experimentID,jenkinsjobname_buildid,k8jobname,podname,Outputgcsbucket,outputfoldername,Seurat1_status,ApprovalStatus) VALUES ('$EXPID','$JENKINSJOBID_$BUILDID','$K8JOBNAME','$podname','NA','NA','Failure','Denied')";
+mysql -h10.60.2.8 -P3306 -ujenkinsuser -pjenkins123 -D tessa_output -e "INSERT INTO seurat1_output_details(experimentID,jenkinsjobname_buildid,k8jobname,podname,Outputgcsbucket,outputfoldername,Seurat1_status,ApprovalStatus) VALUES ('$EXPID','$JENKINSJOBID_$BUILDID','$K8JOBNAME','$podname','$SEURATOUTGCSBUCKET/$SEURATOUTFOLDER','$SEURATOUTFOLDER','Failure','Denied')";
 java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build Seurat-failure-notification -p jenkinsjobID=$JENKINSJOBID-$BUILDID -p k8jobID=seuart1-$BUILDID
 fi

d87a0496263de0966d22b231aafd42fbc75ed6b4
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2020-01-28 16:18:19 +0530

    Update Seurat2_success_failure.sh

--- Seurat/Seurat2_success_failure.sh
+++ Seurat/Seurat2_success_failure.sh
@@ -2,15 +2,17 @@
 BUILDID="$1"
 JENKINSJOBNAME="$2"
 EXPID="$3"
-SEURAT1RDSFOLDERPATH="$4"
-SEURAT1OUTGCSBUCKET="$5"
+SEURAT1OUTGCSBUCKET="$4"
 var1=`echo "$EXPID" | awk '{print tolower($0)}'`;
 expidlower=`echo "$var1" | tr '_' '-'`;
 K8JOBNAME="$expidlower"-"seurat2"-"$BUILDID";
 podname=`hostname`;
 SEURATOUTGCSBUCKET="gs://testinggenomic/Seurat_output"
 SEURAT2OUTFOLDER="seurat2out"-"$EXPID"-"$BUILDID"
-RDSINPUTFILEPATH="/mounttest/$SEURAT1RDSFOLDERPATH/$EXPID.rds"
+rdsbasename=`basename $SEURAT1OUTGCSBUCKET`
+RDSINPUTFOLDERPATH="/mounttest/$rdsbasename/"
+RDSINPUTFILEPATH=`find $RDSINPUTFOLDERPATH -name *.rds`
+
 
 if mkdir $SEURAT2OUTFOLDER && cd $SEURAT2OUTFOLDER && Rscript /mounttest/gitrepo/Seurat/Seurat2.R $RDSINPUTFILEPATH && ls && gsutil cp -r ../$SEURAT2OUTFOLDER $SEURATOUTGCSBUCKET;
 then

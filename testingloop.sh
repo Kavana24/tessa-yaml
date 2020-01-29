10d5f0be291bf7e0f1ccfd886d1c7be3a5b11986
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2020-01-29 15:15:24 +0530

    Delete testingloop.sh

--- testingloop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-x=1
-while [ $x -le 3 ]
-do
-#java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build cellranger-pipeline -p size=100 -p id=1k_PBMC_PUC1 -p transcriptome=/mounttest/PUC_test/reference/refdata-cellranger-GRCh38-3.0.0 -p sample="pbmc_1k_v3" -p fastqs=/mounttest/PUC_test/pbmc_1k_v3_fastqs  -p gcsbucket=gs://testinggenomic/PUC_test
-x=$(($x+1))
-echo $x
-done

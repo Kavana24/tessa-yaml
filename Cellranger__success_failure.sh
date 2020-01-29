e799ff582c7b469ab39e8d22f75ae1b622324d18
Kavana24 <53329624+Kavana24@users.noreply.github.com>
2020-01-29 12:38:43 +0530

    Create Cellranger__success_failure.sh

---
+++ Cellranger__success_failure.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+BUILDID="$1"
+EXPID="$7"
+JENKINSJOBID="$2"
+ID="$3"
+TRANSCRIPTOME="$4"
+FASTQS="$6"
+
+#to obtain the values of sample when not provided in csv
+#cd $FASTQS
+#SAMPLE=`find . -type f -iname "*.fastq.gz" -exec basename "{}" \; | cut -d'_' -f 1-3 | sort -u | tr "\n" "," | sed 's/,$/ /' | tr " " "\n"`
+
+SAMPLE="$5"
+id="$ID"_"$BUILDID";
+podname=`hostname`;
+k8jobname="$EXPID"-"$ID"-cellranger-"$BUILDID";
+
+#i=1;
+#if [ $i -eq 2 ]
+
+if cellranger count --id=$id --transcriptome=$TRANSCRIPTOME --sample=$SAMPLE --fastqs=$FASTQS && ls && gsutil cp -r $id gs://testinggenomic/Cellranger_output ;
+then
+echo "working"
+df -h
+java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build Cellranger-success-notification -p jenkinsjobID=$JENKINSJOBID-$BUILDID -p k8jobID=$k8jobname -p outputgcsbucket=gs://testinggenomic/Cellranger_output/$id -p id=$id -p Experiment_ID=$EXPID -p Podname=$podname
+else
+echo "wrong"
+df -h
+java -jar /jenkins-cli.jar -s http://10.60.2.9:8080/ -auth admin:admin build Cellranger-failure-notification -p jenkinsjobID=$JENKINSJOBID-$BUILDID -p k8jobID=$k8jobname -p id=$id -p Experiment_ID=$EXPID -p Podname=$podname
+fi
